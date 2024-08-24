96d10a39f1df785b2d48b026f6a0206d0cbd004e
santmailo <iamsunil006@gmail.com>
2024-08-24 16:22:36 +0530

    this is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,102 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useContext } from "react";
+import { UserContext } from "../index";
 
+const App = () => {
+  const oj = useContext(UserContext);
+  const {
+    name,
+    setName,
+    list,
+    setList,
+    authenticated,
+    setAuthenticated,
+    userInput,
+    setUserInput,
+  } = useContext(UserContext);
+
+  return (
+    <div>
+      <h1 id="current-user">{`Current user:${name}, isAuthenticated: ${
+        authenticated ? "Yes" : "No"
+      }`}</h1>
+      <button
+        onClick={() => {
+          setName("rohan");
+          setAuthenticated(true);
+        }}
+        id="login-btn"
+      >
+        Login
+      </button>
+      <button
+        id="signout"
+        onClick={() => {
+          setName("");
+          setAuthenticated(false);
+        }}
+      >
+        Signout
+      </button>
+      <br />
+      <br />
+      <input
+        type="text"
+        id="shopping-input"
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          setList([
+            ...list,
+            {
+              item: userInput,
+              idItem: `item-${userInput}`,
+              removeItem: `remove-${userInput}`,
+            },
+          ]);
+        }}
+      >
+        Add Task
+      </button>
+      <button
+        onClick={() => {
+          setList([]);
+        }}
+        id="clear-list"
+      >
+        Clear List
+      </button>
+
+      <br />
+      <br />
+
+      <ul>
+        {list.map((item) => {
+          return (
+            <li id={item.idItem}>
+              {item.item + "  "}
+              <button
+                id={item.removeItem}
+                onClick={() => {
+                  console.log(list);
+                  const newArr = list.filter((fItem) => {
+                    return item.idItem != fItem.idItem;
+                  });
+                  setList(newArr);
+                }}
+              >
+                {" "}
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+export const UserContext = createContext({});
 
+const UserProvider = ({ children }) => {
+  const [name, setName] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+  const [list, setList] = useState([]);
+  const [userInput, setUserInput] = useState("");
 
+  return (
+    <UserContext.Provider
+      value={{
+        name,
+        setName,
+        list,
+
+        setList,
+        authenticated,
+        setAuthenticated,
+        userInput,
+        setUserInput,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+ReactDOM.render(
+  <UserProvider>
+    <App />
+  </UserProvider>,
+  document.getElementById("root")
+);
